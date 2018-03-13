85c2b8b3fa28cccc6230436c8acb232ff68dc008
MatheusDim1 <32242595+MatheusDim1@users.noreply.github.com>
2018-03-13 14:39:32 -0300

    Proxima Aula 20

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = require('./config/server');
+
+/*app.get('/', (req, res) => { 
+    res.send("<html><body> Portal de noticias </body></html>")
+})*/
+
+app.get('/', (req, res) => { 
+    res.render("home/index");
+})
+
+app.get('/formulario_inclusao_noticia', (req, res) => { 
+    res.render("admin/form_add_noticia")
+})
+
+app.get('/noticias', (req, res) => { 
+    res.render("noticias/noticias")
+})
+
+app.listen(3000, () => { 
+    console.log('Servidor ON'); 
+})
+
+
+//Parei na aula 20... 
---
+++ mod_teste.js
@@ -0,0 +1,4 @@
+module.exports = () => {
+    var msg = "Este modulo contem apensa uma string";
+    return msg;    
+}
