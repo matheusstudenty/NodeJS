784e8aeedd5575f7044b31022db2f44dcec694e2
Matheus Rodrigues <matheusstudenty@gmail.com>
2018-03-14 06:25:00 -0300

    Aula 25

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,21 @@
 module.exports = app => {
-    app.get('/noticias', (req, res) => { 
-        res.render("noticias/noticias")
+    
+    app.get('/noticias', (req, res) => {
+        
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({ 
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'portal_noticias'
+        });
+
+        connection.query( 'select * from noticias' ,(error, result) => {
+            res.send(result);
+        });
+
+        
+        //res.render("noticias/noticias")
     });
 };
