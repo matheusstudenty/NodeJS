ec9b3b391e4961d2b62348522f80b4597bba3b99
Matheus Rodrigues <matheusstudenty@gmail.com>
2018-03-21 21:35:48 -0300

    Aula 57

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -10,8 +10,10 @@ module.exports.noticias = function(application, req, res){
 module.exports.noticia = function(application, req, res){
     var connection = application.config.dbConnection();
         var noticiasModel = new application.app.models.NoticiasDAO(connection);
+        
+        var id = req.query;
 
-        noticiasModel.getNoticia((error, result) => {
+        noticiasModel.getNoticia(id, (error, result) => {
             res.render("noticias/noticia", {noticia : result});
         });
 }
